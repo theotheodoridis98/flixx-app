0e39b636c0b23a94b15073848c5fde63556081a7
Thodoris Theodoridis <ttheodoridis@deloitte.gr>
2025-02-10 22:14:49 +0200

    movie details addition

--- js/script.js
+++ js/script.js
@@ -133,6 +133,63 @@ async function displayPopularSeries() {
   });
 }
 
+//Display movie details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+  console.log(movieId);
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  const div = document.createElement('div');
+  div.innerHTML = `<div class="details-top">
+          <div>
+          ${
+            movie.poster_path
+              ? `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.name}"
+          />`
+              : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.name}"
+          />`
+          }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>${movie.genres}</h5>
+            <ul class="list-group">
+              <li>Genre 1</li>
+              <li>Genre 2</li>
+              <li>Genre 3</li>
+            </ul>
+            <a href="#" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $1,000,000</li>
+            <li><span class="text-secondary">Revenue:</span> $2,000,000</li>
+            <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
+            <li><span class="text-secondary">Status:</span> Released</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">Company 1, Company 2, Company 3</div>
+        </div>`;
+
+  document.querySelector('#movie-details').appendChild(div);
+}
+
 // Highlight active navigation link
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
@@ -158,6 +215,7 @@ function init() {
       break;
     case '/movie-details.html':
       console.log('Movie Details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV details');
