3d07648befe6410175a24f0f4541b7ef684860e6
Thodoris Theodoridis <theotheodoridis98@gmail.com>
2025-02-11 18:33:47 +0200

    pagination

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const global = {
     type: '',
     page: 1,
     totalPages: 1,
+    totalResults: 0,
   },
   api: {
     apiKey: 'ae4fecc767d53ab9d4986a4738c0ae24',
@@ -320,8 +321,12 @@ async function search() {
 
   if (global.search.term !== '' && global.search.term !== null) {
     // @todo - make request and display search results
-    const { results, total_pages, page } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
     console.log(results);
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
 
     if (results.length === 0) {
       showAlert('No results found!');
@@ -338,6 +343,11 @@ async function search() {
 
 // Function to display search results
 function displaySearchResults(results) {
+  //Clear previous results
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
+
   results.forEach((result) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -374,7 +384,49 @@ function displaySearchResults(results) {
             </p>
           </div>`;
 
+    document.querySelector('#search-results-heading').innerHTML = `
+      <h2>${results.length} of ${global.search.totalResults} for ${global.search.term}</h2>
+      `;
+
     document.querySelector('#search-results').appendChild(div);
+  });
+
+  displayPagination();
+}
+
+//Create and display pagination for Search
+function displayPagination() {
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML = `
+          <button class="btn btn-primary" id="prev">Prev</button>
+          <button class="btn btn-primary" id="next">Next</button>
+          <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>`;
+
+  document.querySelector('#pagination').appendChild(div);
+
+  //Disable prev button if we are on the first page
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  //Disable next button if we are on the last page
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  //Next Page
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  //Previous Page
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
   });
 }
 
